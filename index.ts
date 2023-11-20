223a58d93fcbc02d80597d4e931fa644fdb45053
Nickolas <nickolasalcazar@gmail.com>
2023-11-20 13:25:25 -0800

    Script now produces a transcript

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log("Hello world");
+import { YoutubeTranscript, TranscriptResponse } from "youtube-transcript";
+
+// Test Youtube video
+const url = "https://www.youtube.com/watch?v=QkP4g9e86qA&ab_channel=exurb1a";
+
+const getTranscript = async (url: string) => {
+  const response: TranscriptResponse[] =
+    await YoutubeTranscript.fetchTranscript(url).then();
+
+  let transcript: string = "";
+
+  // console.log(response);
+
+  response.map((token) => {
+    transcript += " " + token.text;
+  });
+
+  console.log(transcript);
+};
+
+getTranscript(url);
+
+// console.log("Hello world");
